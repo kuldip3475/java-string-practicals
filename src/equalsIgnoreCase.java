7eb02602fb3ca041dda5341a0be1a2e1811bd89c
kuldip3475 <kuldipkadavala73@gmail.com>
2023-02-03 16:09:04 +0530

    true    true   false

---
+++ src/equalsIgnoreCase.java
@@ -0,0 +1,10 @@
+public class equalsIgnoreCase{
+    public static void main(String args[]){
+        String s1="parul";
+        String s2="parul";
+        String s3="PARUL";
+        String s4="python";
+        System.out.println(s1.equalsIgnoreCase(s2));//true because content and case both are same
+        System.out.println(s1.equalsIgnoreCase(s3));//true because case is ignored
+        System.out.println(s1.equalsIgnoreCase(s4));//false because content is not same
+    }}
