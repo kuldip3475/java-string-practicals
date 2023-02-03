140799af94f8bb0b7aede36607dd9cb8ed013399
kuldip3475 <kuldipkadavala73@gmail.com>
2023-02-03 22:32:28 +0530

    hello string   paruluniversity hello stringparuluniversity

---
+++ src/trim.java
@@ -0,0 +1,6 @@
+public class trim{
+    public static void main(String args[]){
+        String s1="  hello string   ";
+        System.out.println(s1+"paruluniversity");
+        System.out.println(s1.trim()+"paruluniversity");
+    }}
